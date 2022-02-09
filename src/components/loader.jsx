dd7ed3c314a9c2ed03408e59af5b32827076e73c
saishanmukkha <saishanmukkha@gmail.com>
2022-02-10 00:00:25 +0530

    0.0.11

---
+++ src/components/loader.jsx
@@ -0,0 +1,16 @@
+export function Loader() {
+  return (
+    <div class="center">
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+    </div>
+  );
+}
