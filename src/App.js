5e6674ba503861cbea3f40c32ca5d5ac2c83774f
saishanmukkha <saishanmukkha@gmail.com>
2022-02-10 16:28:16 +0530

    0.0.13

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import logo from './logo.svg';
 import Projects from './sections/projects';
 import Home from './sections/home';
 import About from './sections/about';
@@ -7,12 +6,12 @@ import Skills from './sections/skills';
 import Contact from './sections/contact';
 import Education from './sections/education';
 import Experience from './sections/experience';
-import Blog from './sections/blog';
 import ParticleBackground from './particle-background';
 import Navigation from './components/navigation';
 import QLinks from './sections/qlinks';
 
 const App = () => {
+  document.title = "Albert Einstein"
   return (
     <div>
       <ParticleBackground></ParticleBackground>
