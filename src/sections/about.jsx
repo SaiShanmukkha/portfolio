e865603542d4799db15c3e3e3effc7816d03ba0b
saishanmukkha <saishanmukkha@gmail.com>
2022-02-11 20:25:24 +0530

    0.0.21

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -7,7 +7,7 @@ export default function About () {
                         <img src="https://docs.google.com/uc?id=10hg5gUwxsCVjKtlr9titl7auLiSwGzFx" className="img-fluid rounded-lg mb-4" alt="profile.png" />
                     </div>
                     <div className="col-lg-8">
-                        <p className="paragraph noselect"><span style={{color:"#fff",fontWeight:"bold"}}>Sai Shanmukkha, Surapaneni</span> is a Active Learner &amp; Cloud Enthusiast who is from Rajahmundry, Andhra Pradesh. He has recently done AWS Developer Associate Certification. He is passionate about working with Cloud technologies. He is also actively looking for opportunities to work with Cloud-based technologies.</p>
+                        <p className="paragraph noselect">I'm <span style={{color:"#fff",fontWeight:"bold"}}>Sai Shanmukkha, Surapaneni</span> who is a Active Learner &amp; Cloud Enthusiast. I'm from Rajahmundry, Andhra Pradesh. I recently done AWS Developer Associate Certification. Also passionate about working with Cloud technologies. Currently, I'm 'actively looking for opportunities to work with Cloud-based technologies.</p>
                         <p className="paragraph noselect">Always eager to learn New Technologies. I’m always open to discussions, so feel free to ask any questions or concerns. Always, happy to collaborate with the community.</p>
                         <p className="paragraph noselect">Currently Leanning Docker, React, etc.</p>                    
                     </div>
